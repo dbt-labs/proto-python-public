09343cccc4c9421cef0660ceb58a12879ea9b6bc
BuildBot <buildbot@fishtownanalytics.com>
2025-05-09 15:39:08 +0000

    Auto-generated from proto v1.0.291

--- setup.py
+++ setup.py
@@ -3,24 +3,26 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-package_name = "dbtlabs_proto_public"
-package_version = "v1.0.290"
+package_name = "dbt-protos"
+package_version = "v1.0.291"
 
 setuptools.setup(
     name=package_name,
     version=package_version,
-    author="dbt Labs",
-    author_email="support@dbtlabs.com",
-    description="Prototypes for the event bus.",
+    author="dbt Labs, Inc.",
+    author_email="info@dbtlabs.com",
+    description="Public proto bindings for dbt",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dbt-labs/proto-python-public",
     packages=setuptools.find_namespace_packages(),
     package_data={"": ["*.pyi", "py.typed"]},
     install_requires=["protobuf>=3.17.1"],
+    license="Apache-2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache-2.0",
     ],
     python_requires=">=3.6",
 )
