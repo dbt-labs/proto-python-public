40cbb0de64bde79764e3d2cb59eb18982df917aa
BuildBot <buildbot@fishtownanalytics.com>
2025-07-17 09:11:08 +0000

    Auto-generated from proto v1.0.344

--- dbtlabs/proto/public/v1/events/vscode_pb2.pyi
+++ dbtlabs/proto/public/v1/events/vscode_pb2.pyi
@@ -88,6 +88,7 @@ class ExtensionLspCompile(google.protobuf.message.Message):
     DBT_VERSION_FIELD_NUMBER: builtins.int
     COMPILE_SUCCESS_FIELD_NUMBER: builtins.int
     ERRORS_FIELD_NUMBER: builtins.int
+    DURATION_MS_FIELD_NUMBER: builtins.int
     project_id: builtins.str
     """this is the hash of the project's name, used for anonymized telemetry"""
     adapter_type: builtins.str
@@ -98,6 +99,8 @@ class ExtensionLspCompile(google.protobuf.message.Message):
     """the version of dbt used in the project"""
     compile_success: builtins.bool
     """Indicates whether the compile was successful"""
+    duration_ms: builtins.int
+    """duration of the compile in milliseconds"""
     @property
     def enrichment(self) -> dbtlabs.proto.public.v1.events.vortex_pb2.VortexMessageEnrichment: ...
     @property
@@ -118,9 +121,10 @@ class ExtensionLspCompile(google.protobuf.message.Message):
         dbt_version: builtins.str = ...,
         compile_success: builtins.bool = ...,
         errors: collections.abc.Iterable[dbtlabs.proto.public.v1.fields.vscode_types_pb2.CompileError] | None = ...,
+        duration_ms: builtins.int = ...,
     ) -> None: ...
     def HasField(self, field_name: typing.Literal["editor", b"editor", "enrichment", b"enrichment", "user", b"user"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing.Literal["adapter_type", b"adapter_type", "adapter_unique_id", b"adapter_unique_id", "compile_success", b"compile_success", "dbt_version", b"dbt_version", "editor", b"editor", "enrichment", b"enrichment", "errors", b"errors", "project_id", b"project_id", "user", b"user"]) -> None: ...
+    def ClearField(self, field_name: typing.Literal["adapter_type", b"adapter_type", "adapter_unique_id", b"adapter_unique_id", "compile_success", b"compile_success", "dbt_version", b"dbt_version", "duration_ms", b"duration_ms", "editor", b"editor", "enrichment", b"enrichment", "errors", b"errors", "project_id", b"project_id", "user", b"user"]) -> None: ...
 
 global___ExtensionLspCompile = ExtensionLspCompile
 
