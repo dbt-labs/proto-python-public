855d47f6b4e8a31b318fc439767dee95477e93ee
BuildBot <buildbot@fishtownanalytics.com>
2026-06-02 12:38:41 +0000

    Auto-generated from proto v1.0.518

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 package_name = "dbt-protos"
-package_version = "v1.0.517"
+package_version = "v1.0.518"
 
 setuptools.setup(
     name=package_name,
